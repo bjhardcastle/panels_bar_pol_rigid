040052ebf287e1d2b4387204f27cf18921a1dddd
bjhardcastle <hardcastle@ucla.edu>
2021-04-15 18:48:55 -0700

    working version

---
+++ panels_barpos_LEDcontrol.ino
@@ -0,0 +1,48 @@
+// switch LED on/off depending on bar position on panels
+// for use during closed-loop bar tracking 
+//
+// use thorlabs ledd1b in 'trigger mode': 
+// - knob on device sets LED voltage
+// - arduino TTL switches on/off
+
+// inputs
+int barInputPin = A0; // input from panels, sampled on A0
+int barPinVal = 0; // raw input val (0-1023)
+int barPos = 0;  // store the bar position (in pixels)
+int barMidlinePos = 49; // (pixels) adjust according to pattern
+int LEDToggleRange = 4; // (+/-pixels) half-width of window in which LED will be on
+
+//outputs
+int LEDOutputPin = 2; // output to LED 
+//int LEDon = 255; // (pwm duty cycle) 255 = 100%
+//int LEDoff = 0; // (pwm duty cycle) 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDOutputPin, OUTPUT);  // sets the pin as output
+  pinMode(LED_BUILTIN, OUTPUT);  // sets the LED as output for testing
+  //Serial.begin(9600); // for testing
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  delay(1);
+  
+  //read bar position (0-1023 <=> 0-5V <=> 1-96pix (for pattern with 96 x-positions
+  barPinVal = analogRead(barInputPin);
+  barPos = ceil((barPinVal*0.0938)); // *96/1024
+  // Serial.println(barPinVal); //for testing 
+  //Serial.println(barPos); //for testing 
+
+  if (barPos >= (barMidlinePos-LEDToggleRange) && barPos <= (barMidlinePos+LEDToggleRange)) {
+    digitalWrite(LEDOutputPin, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH); // for testing
+    // Serial.println("ON"); //for testing 
+
+  }
+  else {
+    digitalWrite(LEDOutputPin, LOW);
+    digitalWrite(LED_BUILTIN, LOW); // for testing
+    // Serial.println("OFF"); //for testing 
+  }
+}
