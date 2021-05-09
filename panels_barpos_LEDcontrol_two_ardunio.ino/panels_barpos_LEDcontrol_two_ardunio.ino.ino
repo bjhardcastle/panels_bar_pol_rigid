9393a9efe64bfb70f4b068814e7b1346c73b65ea
bjhardcastle <hardcastle@ucla.edu>
2021-05-09 09:29:52 -0700

    changed .ino to simplify to single output

--- panels_barpos_LEDcontrol_two_ardunio.ino/panels_barpos_LEDcontrol_two_ardunio.ino.ino
+++ panels_barpos_LEDcontrol_two_ardunio.ino/panels_barpos_LEDcontrol_two_ardunio.ino.ino
@@ -1,13 +1,12 @@
-#include <Wire.h>  // reqd for I2C
 int nidx = 0;      // iteration counter for testing purposes
 // ####################################################################################################
 // switch LED on/off depending on bar position on panels
 // for use during closed-loop bar tracking: TWO ARDUINO SETUP
 //
 // use thorlabs ledd1b in 'MOD mode':
-// - Arduino1 sets voltage level > sends to Arduino2
-// - Arduino2 waits for bar to enter certain position (within window)
-// - Arduino2 sends to voltage level to LED driver via PWM Aout
+// - Arduino1 controls LED and relays digital signal to arduino 2 when bar is within window
+// - Arduino2 waits for bar to enter certain position (within window) 
+// - Arduino2 bypasses arduino1 CL control and manually toggles LED
 // ####################################################################################################
 
 // inputs
@@ -18,9 +17,9 @@ int barPos = 0;           // store the bar position (in pixels)
 int barMidlinePos = 49;   // (pixels) adjust according to pattern
 int LEDToggleRange = 2;   // (+/-pixels) half-width of window in which LED will be on
 
-int LEDVoltageInputPin = A7;  // input from Aout on Arduino1 (connected to Matlab), specify LED voltage
-int LEDVoltagePinVal = 0;     // (from pwm duty cycle) 0-1023 (read) => 0-255 (sent) => 0-100%
-int LEDVoltage = 0;           // store the desired LED voltage (0-5V)
+// int LEDVoltageInputPin = A7;  // input from Aout on Arduino1 (connected to Matlab), specify LED voltage
+// int LEDVoltagePinVal = 0;     // (from pwm duty cycle) 0-1023 (read) => 0-255 (sent) => 0-100%
+int LEDVoltage = 5;           // store the desired LED voltage (0-5V)
 
 int bypassInputPin = 2;  // input from another TTL source to bypass LED gating
 int bypassToggle = LOW;  // toggle HIGH (bypass) or LOW (normal gating mode)
@@ -28,8 +27,8 @@ int bypassToggle = LOW;  // toggle HIGH (bypass) or LOW (normal gating mode)
 int manualONOFFInputPin = 3;  // input from another TTL source to manually switch LED on at specified voltage
 int manualONOFFToggle = LOW;  // toggle HIGH to switch ON (bypassPinVal must also be HIGH)
 
-int resetBarTimeInputPin = 4;  // input from another TTL source to bypass LED gating
-int resetBarTimeToggle = LOW;  // toggle HIGH to reset device
+// int resetBarTimeInputPin = 4;  // input from another TTL source to bypass LED gating
+// int resetBarTimeToggle = LOW;  // toggle HIGH to reset device
 
 // ####################################################################################################
 
@@ -38,13 +37,16 @@ int resetBarTimeToggle = LOW;  // toggle HIGH to reset device
 int LEDOutputPin = A2;    // output to LED (pwm duty cycle) 0-255 => 0-100%
 int LEDOutputPinVal = 0;  // value to be sent (0-255)
 
-int barTimeOutputPin = A1;  // output to Arduino1 (connected to Matlab), specifying bar time spent within window
-int barTimePinVal = 0;      // 0-255 (sent) => 0-255s
-int barTimeSumS = 150;      // (sec) counter 0-255, counts cumulative time within window, wraps around when 255 reached
-int barTimeSumMS = 0;       // (ms) counter 0-999, counts ms within window since last integer number of seconds
+// int barTimeOutputPin = A1;  // output to Arduino1 (connected to Matlab), specifying bar time spent within window
+// int barTimePinVal = 0;      // 0-255 (sent) => 0-255s
+// int barTimeSumS = 150;      // (sec) counter 0-255, counts cumulative time within window, wraps around when 255 reached
+// int barTimeSumMS = 0;       // (ms) counter 0-999, counts ms within window since last integer number of seconds
 
-int barTimeMultiplyOutputPin = A3;  // output to Arduino1 (connected to Matlab), specifying bar time spent within window
-int barTimeMultiplyVal = 240;       // 0-255 (used to multiply value on barTimePinVal)
+// int barTimeMultiplyOutputPin = A3;  // output to Arduino1 (connected to Matlab), specifying bar time spent within window
+// int barTimeMultiplyVal = 240;       // 0-255 (used to multiply value on barTimePinVal)
+
+int barInWindowOutpuPin = 4;  // input from another TTL source to manually switch LED on at specified voltag
+int barInWindow = LOW;  // toggle HIGH to switch ON (bypassPinVal must also be HIGH)
 
 // ####################################################################################################
 
@@ -52,24 +54,22 @@ void setup() {
   // put your setup code here, to run once:
 
   pinMode(barPosInputPin, INPUT);
-  pinMode(LEDVoltageInputPin, INPUT);
+//   pinMode(LEDVoltageInputPin, INPUT);
   pinMode(LEDOutputPin, OUTPUT);              // sets the pin as output
-  pinMode(barTimeOutputPin, OUTPUT);          // sets the pin as output
-  pinMode(barTimeMultiplyOutputPin, OUTPUT);  // sets the pin as output
+//   pinMode(barTimeOutputPin, OUTPUT);          // sets the pin as output
+//   pinMode(barTimeMultiplyOutputPin, OUTPUT);  // sets the pin as output
   pinMode(LED_BUILTIN, OUTPUT);               // sets the LED as output for testing
-  pinMode(0, INPUT);                          // sets the pin as output
-  pinMode(1, INPUT);                          // sets the pin as output
   pinMode(2, INPUT);                          // sets the pin as output
+  pinMode(3, INPUT);                          // sets the pin as output
+  pinMode(4, OUTPUT);                          // sets the pin as output
 
   Serial.begin(115200);  // for testing
                          // Serial.begin(115200); // for testing
   Serial.print("\n\tnano reset\n");
 
-  Wire.begin(0);
-
   // at start, read desired LED voltage from matlab-connected Arduino1 and setup for output to LED on pwm pin
-  LEDVoltagePinVal = analogRead(LEDVoltageInputPin);  // 0-1023 (read) => 0-255 (sent) => 0-100%
-  LEDVoltage = LEDVoltagePinVal * 0.00488;            // 5/1023
+//   LEDVoltagePinVal = analogRead(LEDVoltageInputPin);  // 0-1023 (read) => 0-255 (sent) => 0-100%
+//   LEDVoltage = LEDVoltagePinVal * 0.00488;            // 5/1023
   //Serial.println(LEDVoltage); //for testing
   LEDOutputPinVal = LEDVoltage * 51;  //255/5
 
